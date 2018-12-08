c7cb81550b522f8b78e1947a0f93a6eaf36a9f15
bobos1208 <bobos1208@gmail.com>
2018-12-08 18:28:54 +0900

    edit hello.py
    I edit some code from Taesoo han for mor fun!

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-for i in range (1, 5+1):
-    print("It works on subdim!")
+for i in range (1, 10+1):
+    if i % 2 == 0:
+        print("Yes, this works on even number.!")
+    elif i % 3 == 0:
+        print("Not bad, yet we love even.!")
+    else: 
+        print("Oops, try again, yet thanks Taesoo Han for basic code.!")
 
